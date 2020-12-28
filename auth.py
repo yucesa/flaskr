441c9ac09cb7a76b12262ccded0f70eb666f799c
yucesa <cs_y@foxmail.com>
2020-12-28 15:14:50 +0800

    fixed some bugs

--- auth.py
+++ auth.py
@@ -72,7 +72,7 @@ def load_logged_in_user():
     user_id = session.get('user_id')
 
     if user_id is None:
-        g.user_id = None
+        g.user= None
     else:
         g.user = get_db().execute(
             'SELECT * FROM user where id = ?', (user_id,)
